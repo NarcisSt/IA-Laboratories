47100db5f0f309e4c757831b8d2e9620f9ede360
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-11-02 19:54:14 +0200

    Added lab 5-6 parts 3 and 4

--- lab5-6/lab5-6.py
+++ lab5-6/lab5-6.py
@@ -1,49 +1,109 @@
 import random
 
 
-class State:
-    steps = 0
-    n = 0
-    m = 0
+# Function to print the mastermind board
+def print_mastermind_board(passcode, guess_codes, guess_flags, k):
+
+    print("    |", end="")
+    for x in passcode:
+        print("\t" + x[:3], end="")
+    print()
+
+    for i in reversed(range(len(guess_codes))):
+        print("------------------------------")
+        print(guess_flags[i][0], guess_flags[i][1], "|")
+
+        print(guess_flags[i][2], guess_flags[i][3], end=" |")
+        for x in guess_codes[i]:
+            print("\t" + x[:3], end="")
+
+        print()
+    print("------------------------------")
+    print("Number of right colors: " + str(k))
+
+
+def generate_random_code(colorsArray):
+    random.shuffle(colorsArray)
+    return colors[:4]
+
+
+def compareCodes(passcode1, dummy_passcode1, code1, colors_map1):
     k = 0
-
-    def __init__(self, n, m, k):
-        self.n = n
-        self.m = m
-        self.k = k
-        self.steps = 0
-
-
-def getInitialState(n, m, k):
-    return State(n, m, k)
+    for x in code:
+        if colors_map1[x] in dummy_passcode1:
+            if code1.index(x) == passcode1.index(colors_map[x]):
+                k += 1
+    return k
 
 
-def isFinal(state):
-    if state.steps > 2 * state.n:
-        return True  # A a castigat
-    else:
-        return False  # B a castigat
-
-
-def countFromArray(array, n):
-    return array.count(n)
-
-
-def selectRandom(state):
-    numbers = random.randrange(1, state.m * state.n)
-    lst1 = []
-    for value in range(numbers):
-        aux1 = random.randrange(state.n)
-        first_tuple_elements = [a_tuple[0] for a_tuple in lst1]
-        if countFromArray(first_tuple_elements, aux1) < state.m:
-            lst2 = [aux1, random.randrange(1, state.m)]
-            lst1.append(lst2)
-        else:
-            lst2 = [random.randrange(1, state.n), random.randrange(1, state.m)]
-            lst1.append(lst2)
-    return lst1
-
-
+# The Main function
 if __name__ == '__main__':
-    initialState = getInitialState(4, 3, 2)
-    print(selectRandom(initialState))
+
+    colors = ["RED", "GREEN", "YELLOW", "BLUE", "BLACK", "ORANGE"]
+    colors_map = {1: "RED", 2: "GREEN", 3: "YELLOW", 4: "BLUE", 5: "BLACK", 6: "ORANGE"}
+
+    passcode = generate_random_code(colors)
+
+    chances = 8
+
+    show_passcode = ['UNK', 'UNK', 'UNK', 'UNK']
+
+    guess_codes = [['-', '-', '-', '-'] for x in range(chances)]
+
+    guess_flags = [['-', '-', '-', '-'] for x in range(chances)]
+
+    turn = 0
+    k = 0
+    while turn < chances:
+
+        print("\n\n1 - RED, 2 - GREEN, 3 - YELLOW, 4 - BLUE, 5 - BLACK, 6 - ORANGE\n\n")
+        print_mastermind_board(show_passcode, guess_codes, guess_flags, k)
+
+        try:
+            code = list(map(int, input("Enter the code : ").split()))
+        except ValueError:
+            print("\tPlease enter a valid code!")
+            continue
+
+        if len(code) != 4:
+            print("\tPlease enter a valid code!")
+            continue
+
+        flag = 0
+        for x in code:
+            if x > 6 or x < 1:
+                flag = 1
+
+        if flag == 1:
+            print("\tPlease enter a valid code!")
+            continue
+
+        for i in range(4):
+            guess_codes[turn][i] = colors_map[code[i]]
+
+        dummy_passcode = [x for x in passcode]
+
+        pos = 0
+
+        k = compareCodes(passcode, dummy_passcode, code, colors_map)
+
+        for x in code:
+            if colors_map[x] in dummy_passcode:
+                if code.index(x) == passcode.index(colors_map[x]):
+                    guess_flags[turn][pos] = 'K'
+                pos += 1
+                dummy_passcode.remove(colors_map[x])
+
+        random.shuffle(guess_flags[turn])
+
+        if guess_codes[turn] == passcode:
+            print_mastermind_board(passcode, guess_codes, guess_flags, k)
+            print("You win! :)")
+            break
+
+        turn += 1
+        
+
+if turn == chances:
+    print_mastermind_board(passcode, guess_codes, guess_flags, k)
+    print("Game over! :(")
--- lab5-6/readme.md
+++ lab5-6/readme.md
@@ -15,13 +15,43 @@ Dacă după cel mult 2*n pași jucătorul B a propus secvența de culori corespu
 
 - &#9745; 1. (0.2) Implementați funcția de inițializare (primește ca parametrii n, m și k, întoarce starea inițială) și funcția care verifică dacă o stare primită ca parametru este finală, caz în care întoarce câștigătorul.
 - &#9745; 2. (0.2) Implementați o funcție care alege aleator k bile din cele m*n disponibile.
-- &#9744; 3. (0.2) Implementați o funcție care compară o secvență de k culori cu secvența generată de funcția anterioară și întoarce o valoare între 0 și k corespunzătoare numărului de potriviri între cele două șiruri.
-- &#9744; 4. (0.3) Implementați o interfață pentru jucătorul B uman, cu posibilitatea introducerii unei secvențe de culori, afișarea numărului de potriviri și afișarea câștigătorului jocului.
+- &#9745; 3. (0.2) Implementați o funcție care compară o secvență de k culori cu secvența generată de funcția anterioară și întoarce o valoare între 0 și k corespunzătoare numărului de potriviri între cele două șiruri.
+- &#9745; 4. (0.3) Implementați o interfață pentru jucătorul B uman, cu posibilitatea introducerii unei secvențe de culori, afișarea numărului de potriviri și afișarea câștigătorului jocului.
 - &#9744; 5. (0.7) Implementați o strategie pentru jucătorul B folosind algoritmul MINIMAX cu retezarea ALPHA-BETA.
 
 The checkbox means that the problem was solved.
 
 # Output
 ```bash
-TBD
+1 - RED, 2 - GREEN, 3 - YELLOW, 4 - BLUE, 5 - BLACK, 6 - ORANGE
+
+
+    |	UNK	UNK	UNK	UNK
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+- - |	-	-	-	-
+------------------------------
+- - |
+K - |	RED	GRE	YEL	BLU
+------------------------------
+Number of right colors: 1
+Enter the code : 
 ```
